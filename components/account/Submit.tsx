0343480569d9baf6880cdecdb3b636f1e26ac41b
ityeony00 <ityeony@gmail.com>
2025-10-19 09:45:30 +0900

    feat: add signup form with validation

---
+++ components/account/Submit.tsx
@@ -0,0 +1,12 @@
+//app/(account)/components/account/Submit.tsx
+import { Button } from "@/components/ui/button";
+
+type ButtonProps = React.ComponentProps<typeof Button>;
+
+export function Submit({ children, ...others }: ButtonProps) {
+  return (
+    <Button type="submit" variant="destructive" {...others}>
+      {children}
+    </Button>
+  );
+}
