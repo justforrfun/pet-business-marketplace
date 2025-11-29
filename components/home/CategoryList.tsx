1f53a90766ae1406381a4484cb457141277301f4
pu1et <pu1etproof@gmail.com>
2025-11-29 20:51:42 +0900

    refactor: 배너 리스트 컴포넌트 크기  카테고리별 상품 컴포넌트와 맞추기

--- components/home/CategoryList.tsx
+++ components/home/CategoryList.tsx
@@ -1,66 +1,68 @@
-'use client';
+"use client";
 
 import { supabase } from "@/lib/supabaseClient";
 import { useEffect, useState } from "react";
-import ProductList from "./ProductList";
+import ProductList from "./ProductList"
 
 interface Category {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export default function CategoryList() {
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
 
-    useEffect(() => {
-        const fecthCategories = async () => {
-            const { data, error } = await supabase
-                .from('category')
-                .select('id, name')
-                .order('id', { ascending: true });
+  useEffect(() => {
+    const fecthCategories = async () => {
+      const { data, error } = await supabase
+        .from("category")
+        .select("id, name")
+        .order("id", { ascending: true });
 
-            if (error) {
-                console.error('Error fetching categories:', error.message);
-            } else {
-                setCategories(data || []);
-            }
-        };
+      if (error) {
+        console.error("Error fetching categories:", error.message);
+      } else {
+        setCategories(data || []);
+      }
+    };
 
-        fecthCategories();
-    }, []);
+    fecthCategories();
+  }, []);
 
-    return (
-        <section className="mt-12 px-10">
-            <h2 className="text-xl font-bold mb-6 text-gray-800">카테고리별 상품</h2>
+  return (
+    <section className="mt-12 px-10">
+      <h2 className="text-xl font-bold mb-6 text-gray-800">카테고리별 상품</h2>
 
-            { /* 카테고리 버튼 */}
-            <div className="flex gap-2 mb-10">
-                <button
-                    onClick={() => setSelectedCategory(null)}
-                    className={`px-4 py-2 rounded-md text-sm font-medium ${selectedCategory === null
-                        ? 'bg-red-700 text-white'
-                        : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
-                        }`}
-                >
-                    전체
-                </button>
-                {categories.map((category) => (
-                    <button
-                        key={category.id}
-                        onClick={() => setSelectedCategory(category.id)}
-                        className={`px-4 py-2 rounded-md text-sm font-medium ${selectedCategory === category.id
-                            ? 'bg-red-700 text-white'
-                            : 'border-1 border-gray-200 bg-white text-gray hover:bg-gray-200'
-                            }`}
-                    >
-                        {category.name}
-                    </button>
-                ))}
-            </div>
+      {/* 카테고리 버튼 */}
+      <div className="flex gap-2 mb-10">
+        <button
+          onClick={() => setSelectedCategory(null)}
+          className={`px-4 py-2 rounded-md text-sm font-medium ${
+            selectedCategory === null
+              ? "bg-red-700 text-white"
+              : "bg-gray-100 text-gray-700 hover:bg-gray-200"
+          }`}
+        >
+          전체
+        </button>
+        {categories.map((category) => (
+          <button
+            key={category.id}
+            onClick={() => setSelectedCategory(category.id)}
+            className={`px-4 py-2 rounded-md text-sm font-medium ${
+              selectedCategory === category.id
+                ? "bg-red-700 text-white"
+                : "border-1 border-gray-200 bg-white text-gray hover:bg-gray-200"
+            }`}
+          >
+            {category.name}
+          </button>
+        ))}
+      </div>
 
-            { /* 상품 목록 */}
-            <ProductList selectedCategory={selectedCategory} />
-        </section>
-    );
+      {/* 상품 목록 */}
+      <ProductList selectedCategory={selectedCategory} />
+    </section>
+  );
 }
